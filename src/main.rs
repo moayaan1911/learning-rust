8277f558faeffbf6d8b1059b9116384ad1bea18b
moayaan1911 <ayaangames@gmail.com>
2024-07-18 09:22:52 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // let num:u8 = 10;
+    // println!("Hello, China! {}", num);
+
+    // STRING
+    // let string_literal_static : &str = "Hello Everyone"; // stored in stack special memory, its static
+    // println!( "{}", string_literal_static);
+
+    // let mut string_literal_dynamic : String = String::from("Hello Everyone"); // String stored in heap, its dynamic
+    // string_literal_dynamic.push_str(", China!");
+    // println!( "{}", string_literal_dynamic);
+
+    // TUPLE
+    // let emp_value:(&str, u8) = ("Tom", 32);
+    // println!("Employee name is {} and age is {}", emp_value.0, emp_value.1);
+
+    // FUNCTIONS
+    // let number1: u8 = 10;
+    // let number2: u8 = 20;
+    // let result: u8 = sum(number1, number2);
+    // println!("Sum of {} and {} is {}", number1, number2, result);
+}
+
+// fn sum(number1: u8, number2: u8) -> u8 {
+//     return number1 + number2;
+// }
